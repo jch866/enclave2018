727b2c5131fbb891c3050f9a67028aa93244ef34
jch866 <jch866@126.com>
2018-04-23 09:01:13 +0800

    把三个JS文件合并到main.js中

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -16,10 +16,11 @@ $(function() {
         mWeChat = $('#m_weChat'),
         mask = $('#mask'),
         arrow = $(".arrow");
-    var downurl={iOS:'https://itunes.apple.com/cn/app/%E9%A3%9E%E5%9C%B0-%E6%96%87%E8%89%BA%E9%9D%92%E5%B9%B4%E7%9A%84%E9%AB%98%E5%93%81%E8%B4%A8%E6%96%87%E5%AD%A6%E8%89%BA%E6%9C%AF%E9%98%85%E8%AF%BB%E7%A4%BE%E5%8C%BA/id1179249797?mt=8',
-            android:'http://a.app.qq.com/o/simple.jsp?pkgname=cn.enclavemedia.app&from=singlemessage',
-            index:'index.html'
-        };
+    var downurl = {
+        iOS: 'https://itunes.apple.com/cn/app/%E9%A3%9E%E5%9C%B0-%E6%96%87%E8%89%BA%E9%9D%92%E5%B9%B4%E7%9A%84%E9%AB%98%E5%93%81%E8%B4%A8%E6%96%87%E5%AD%A6%E8%89%BA%E6%9C%AF%E9%98%85%E8%AF%BB%E7%A4%BE%E5%8C%BA/id1179249797?mt=8',
+        android: 'http://a.app.qq.com/o/simple.jsp?pkgname=cn.enclavemedia.app&from=singlemessage',
+        index: 'index.html'
+    };
     var delay = 60 * 60 * 3 * 1000; //3小时缓存机制
     var cache = { aL: 'artList', aLT: 'artList_time' };
     var len1 = 6,
@@ -53,9 +54,9 @@ $(function() {
         specialarticle: 'http://test.enclavebooks.cn/v2/shareSpecialArticle?id=9',
         article: 'http://test.enclavebooks.cn/v2/shareArticle?id=253',
     };
-    var article_v1 = [{ id: 'new_article_v1', url: url.article},
-        { id: 'special_aritcle_v1', url: url.specialarticle},
-        { id: 'special_v1', url: url.special}
+    var article_v1 = [{ id: 'new_article_v1', url: url.article },
+        { id: 'special_aritcle_v1', url: url.specialarticle },
+        { id: 'special_v1', url: url.special }
     ];
     article_v1.forEach(function(item) {
         $('#' + item.id).length > 0 && getRes_v1(item);
@@ -376,13 +377,13 @@ $(function() {
         mask.fadeIn(100);
         mask.parent().addClass('scrollLimit')
         doc.on("touchmove", function(e) {
-             if (e.cancelable) {
+            if (e.cancelable) {
                 // 判断默认行为是否已经被禁用
                 if (!e.defaultPrevented) {
                     e.preventDefault();
                 }
             }
-           // e.preventDefault();
+            // e.preventDefault();
         });
     }
 
@@ -492,9 +493,9 @@ $(function() {
         } else if (device == 'iOS_wechat') {
             showMask();
         } else if (device == 'Android' || device == 'Android_wechat') {
-            window.location.href = downurl.android ;
+            window.location.href = downurl.android;
         } else {
-            window.location.href = downurl.index ;
+            window.location.href = downurl.index;
         }
     }
     downApp && downApp.on('click', function(e) {
@@ -509,12 +510,12 @@ $(function() {
 
     //新版 通用请求api
     function getRes_v1(item) {
-        $('#'+item.id).show().addClass('loadingBg');
+        $('#' + item.id).show().addClass('loadingBg');
         $.get(item.url, function(data) {
-            var obj = { id: item.id,data:data,callback:afterRender};
+            var obj = { id: item.id, data: data, callback: afterRender };
             if (data.code == 200) {
                 new enclave_rt(obj);
-                $('#'+item.id).hide().removeClass('loadingBg');
+                $('#' + item.id).hide().removeClass('loadingBg');
             } else {
                 window.location.href = 'error.html';
             }
@@ -525,43 +526,372 @@ $(function() {
         new m_audio({ sel: $('.el-audio') });
         otherOperate_v1();
     }
-    function otherOperate_v1(){
+
+    function otherOperate_v1() {
         var mart_head = $('.mdetail_head');
         var downApp = $('#downApp'); //顶部直接下载
         var lock_buy = $('#lock_buy'); //有锁的地方
-        var order_buy = $('#order_buy');//专栏订阅
-        var openApp = $('#openApp');//打开
+        var order_buy = $('#order_buy'); //专栏订阅
+        var openApp = $('#openApp'); //打开
         (mart_head.length != 0) && scrollFixed(mart_head);
-        (downApp.length != 0 )&& downApp.on('click', function(e) {
+        (downApp.length != 0) && downApp.on('click', function(e) {
             e.preventDefault();
             downLoad();
         });
-        (lock_buy.length != 0 )&& lock_buy.on('click', function(e) {
+        (lock_buy.length != 0) && lock_buy.on('click', function(e) {
             e.preventDefault();
             downLoad();
         });
-        (order_buy.length != 0 )&& order_buy.on('click', function(e) {
+        (order_buy.length != 0) && order_buy.on('click', function(e) {
             e.preventDefault();
             downLoad();
         });
-        (openApp.length != 0 )&& openApp.on('click', function(e) {
+        (openApp.length != 0) && openApp.on('click', function(e) {
             applink();
         })
 
     }
-    function applink(){ 
+
+    function applink() {
         var device = devicePlatform();
         if (device == 'iOS') {
-            setTimeout(function(){
+            setTimeout(function() {
                 window.location.href = downurl.iOS
                 window.location.href = downurl.iOS
-            },2000)
+            }, 2000)
         } else if (device == 'Android') {
-            setTimeout(function(){
-                 window.location.href = downurl.android ;
-            },2000)
+            setTimeout(function() {
+                window.location.href = downurl.android;
+            }, 2000)
         }
-    } 
+    }
 
 
 });
+//渲染文章内容
+$(function() {
+    function renderTemplate(options) {
+        var defaults = {
+            id: 'new_article_v1', // new_article_v1  special_v1  special_aritcle_v1 
+            data: options.data,
+            homepage: 'https://app.enclavebooks.cn',
+            articleBody: ''
+        };
+        this.articleWrap = $('<article class="shareArt"><div class="padding" id="articleCon"></div></article>');
+        this.settings = $.extend(defaults, options);
+        this._callback = this.settings.callback;
+        this.body = $('#' + this.settings.id).parents('body');
+        this.renderAll();
+        this._callback && this._callback();
+    }
+    renderTemplate.prototype.renderAll = function() {
+        var data = this.settings.data;
+        this._v1_header(data);
+        this.body.append(this.articleWrap); //复用;
+        this.settings.articleBody = this.body.find('#articleCon');
+        if (this.settings.id == 'new_article_v1') {
+            this._v1_article_body(data);
+            this._v1_article_comment(data);
+        } else if (this.settings.id == 'special_v1') {
+            this._v1_special_body(data);
+            this._v1_special_preview(data);
+            this._v1_article_comment(data); //和文章评论一样的DOM，复用
+            this._v1_special_subscribe(data);
+        } else if (this.settings.id == 'special_aritcle_v1') {
+            this._v1_specialArt_body(data);
+            this._v1_specialArt_comment(data);
+        }
+
+    };
+    renderTemplate.prototype._v1_header = function(d) {
+        var data = d.result;
+        var currentPage = this.settings.id;
+        var id = '';
+        if (currentPage === 'new_article_v1') {
+            id = data.article.artId
+        } else if (currentPage === 'special_v1') {
+            id = data.special.id
+        } else if (currentPage === 'special_aritcle_v1') {
+            id = data.artId
+        }
+        var link = {
+            new_article_v1: 'enclave://article?id=' + id,
+            special_v1: 'enclave://_special?id=' + id,
+            special_aritcle_v1: 'enclave://special_article?id=' + id
+        }
+        var str = '<header class="mdetail_head">' +
+            '<div class="head_wrap">  ' +
+            '<div class="art_logo">' +
+            '<a href="index.html"><img src="img/logo_new.svg" alt=""></a>' +
+            '</div>' +
+            '<div class="down_btn">' +
+            '<a href="javascript:void(0)" class="download" id="downApp">直接下载</a>' +
+            '<a href="' + link[currentPage] + '" class="downApp" id="openApp">打开</a>' +
+            '</div>' +
+            '</div>' +
+            '</header>';
+        this.body.append($(str))
+    };
+    //付费文章页 
+    renderTemplate.prototype._v1_specialArt_body = function(d) {
+        var data = d.result;
+        var self = this;
+        var str = '<img src="' + data.artThumb + '" alt="">' +
+            '<h1>' + data.artTitle + '</h1>' +
+            '<div class="avatar_wrap">' +
+            '<span class="avatar">' +
+            '<img src="' + data.artAvatar + '" alt="">' +
+            '</span>' +
+            '<span class="profile">' +
+            '<div class="name blue">' + data.artEditor + '</div>' +
+            '<div class="time">' + self.format(data.artTime * 1000) + '</div>' +
+            '</span>' +
+            '</div>' + data.artDescription;
+        '</div>';
+        var lock = '<div class="lock_tip" id="lock_buy">' +
+            '<div class="img"><img src="img/unlock.svg" alt=""></div>' +
+            '<div class="buy_text">购买专栏阅读剩余内容</div>' +
+            '</div>';
+        self.settings.articleBody.append($(str));
+        this.body.append($(lock));
+    };
+    renderTemplate.prototype._v1_specialArt_comment = function(d) {
+        var data = d.result.special;
+        var specialWrap = '<div class="lock_owner"><div class="title">所属专栏</div><div class="l_box specialOwner"></div></div>';
+        //this.settings.articleBody.append($(specialWrap));
+        this.body.append($(specialWrap));
+        var specialOwner = this.body.find('.specialOwner');
+        var str = '<div class="l_thumb">' +
+            '<img src="' + data.thumb + '" alt="">' +
+            '</div>' +
+            '<div class="l_intro">' +
+            '<h4>' + data.title + '</h4>' +
+            '<p>' + data.intro.slice(0, 50) + '</p>' +
+            '<div class="l_foot">' +
+            '<span class="lf_name">' + data.authorName + '</span>' +
+            '<span class="lf_price">' + data.afterDiscountPrice + '飞币<del>' + data.originalPrice + '飞币</del></span>' +
+            '</div>' +
+            '</div>';
+        specialOwner.append($(str));
+    };
+    //专栏页 
+    renderTemplate.prototype._v1_special_body = function(d) {
+        var data = d.result.special;
+        var self = this;
+        var str = '<div class="m_special_header">' +
+            '<img src="' + data.thumb + '" alt="">' +
+            '<div class="m_special_title">' +
+            '<h1>' + data.title + '</h1>' +
+            '<span>' + self.format(data.time * 1000) + '</span>' +
+            '</div>' +
+            '</div>' + data.specialColumnIntro;
+        self.settings.articleBody.append($(str));
+    };
+    renderTemplate.prototype._v1_special_preview = function(d) {
+        var data = d.result.special.preview;
+        var self = this;
+        var wrap = '<div class="commentBox"><div class="m_preview"><h1>试读</h1><ul class="m_preview_list"></ul></div></div>';
+        this.body.append($(wrap));
+        var preview = this.body.find('.m_preview_list');
+        $.each(data, function(index, item) {
+            var str = '<a href="special_article.html?art_id=' + item.artId + '" ><li>' +
+                '<div class="summary">' +
+                '<h4>' + item.artTitle + '</h4>' +
+                '<p>' + item.artTitle + '</p>' +
+                '</div>' +
+                '<div class="thumbnail">' +
+                '<img src="' + item.artThumb + '" alt="">' +
+                '</div>' +
+                '</li></a>';
+            preview.append($(str));
+        })
+    };
+    renderTemplate.prototype._v1_special_subscribe = function(d) {
+        var data = d.result.special;
+        var str = '<div class="m_subscribe_bar">' +
+            '<div class="price">' + data.afterDiscountPrice + '飞币<del>' + data.originalPrice + '飞币</del></div>' +
+            '<div class="order" id="order_buy">订阅</div>' +
+            '</div>';
+        this.body.append($(str));
+    };
+    //文章详情页 
+    renderTemplate.prototype._v1_article_body = function(d) {
+        var data = d.result.article;
+        var self = this;
+        var str = '<img src="' + data.artThumb + '" alt=""><h1>' + data.artTitle + '</h1>' +
+            '<div class="avatar_wrap">' +
+            '<span class="avatar">' +
+            '<img src="' + data.artAvatar + '" alt="">' +
+            '</span>' +
+            '<span class="profile">' +
+            '<div class="name blue">' + data.artEditor + '</div>' +
+            '<div class="time">' + self.format(data.artTime * 1000) + '</div>' +
+            '</span>' +
+            '</div>' + data.artContent.replace(/\/ueditor\/php/g, (self.homepage + "/ueditor/php"));
+        self.settings.articleBody.append($(str));
+    };
+    renderTemplate.prototype._v1_article_comment = function(d) {
+        var data = d.result.comment;
+        var self = this;
+        var wrap = '<div class="commentBox"><div class="m_comment">' +
+            '<h1>最新评论</h1><ul class="m_comment_list"></ul>' +
+            '</div></div>';
+        this.body.append($(wrap));
+        var comment = this.body.find('.m_comment_list');
+        $.each(data, function(index, item) {
+            var str = '<li>' +
+                '<div class="m_header avatar_wrap">' +
+                '<span class="avatar"><img src="' + item.avatar + '" alt=""></span>' +
+                '<span class="profile">' +
+                '<div class="name">' + item.name + '</div>' +
+                '<div class="time">' + self.format(item.time * 1000) + '</div>' +
+                '</span>' +
+                '</div>' +
+                '<div class="m_body">' + item.content + '</div>' +
+                '</li>'
+            comment.append($(str));
+        })
+    };
+    renderTemplate.prototype.format = function(seconds) {
+        //seconds是整数，否则要parseInt转换
+        var time = new Date(seconds);
+        var y = time.getFullYear();
+        var m = time.getMonth() + 1;
+        var d = time.getDate();
+        var h = time.getHours();
+        var mm = time.getMinutes();
+        var s = time.getSeconds();
+        var add0 = function add0(m) { return m < 10 ? '0' + m : m }
+        return y + '-' + add0(m) + '-' + add0(d) + ' ' + add0(h) + ':' + add0(mm) + ':' + add0(s);
+    };
+    window.enclave_rt = renderTemplate;
+}(window));
+//处理音频播放
+$(function() {
+    function m_audio(cfg) {
+        this.allAudio_P = cfg.sel;
+        if (this.allAudio_P.length === 0) return;
+        this.currentAudio = '';
+        this.currentAudioJS = '';
+        this.currentAudioId = '';
+        this.currentAudioUrl = '';
+        this.currentAudioOriginalUrl = '';
+        this.currentAudioDuration = '';
+        this.currentAudioSpanChild = '';
+        this.removePlay = cfg.sel.find('img.el-audio-play');
+        this.removeDel = cfg.sel.find('img.el-audio-delete');
+        this.init();
+    };
+    m_audio.prototype = {
+        init: function() {
+            this.removePlay.remove();
+            this.removeDel.remove();
+            this._initDurationTime();
+            this.renderAudioDom();
+            this.bindAudioEvent();
+        },
+        _initDurationTime: function() {
+            var self = this;
+            this.allAudio_P.each(function(idx) {
+                var time = $(this).attr('data-duration');
+                $(this).find('span.el-audio-duration').text(self._formatDuration(time))
+            })
+        },
+        renderAudioDom() {
+            var self = this;
+            this.allAudio_P.addClass('audio-play-box mp3_play');
+            this.allAudio_P.each(function(idx) {
+                var $this = $(this);
+                self._setCurrentAudioInfo($this); //设备当前音频的信息
+                var audio = new Audio();
+                var attrObj = {
+                    'data-id': self.currentAudioId,
+                    'data-url': self.currentAudioUrl,
+                    'data-original-url': self.currentAudioOriginalUrl,
+                    'src': self.currentAudioUrl,
+                    'loop': false,
+                    'hidden': true
+                }
+                $(audio).attr(attrObj);
+                $this.append($(audio));
+            })
+        },
+        bindAudioEvent() {
+            var self = this;
+            self.allAudio_P.each(function(idx) {
+                var $this = $(this);
+                $this.on('click', function() {
+                    self._setCurrentAudioInfo($this); //设备当前音频的信息
+                    //console.log(self.currentAudioId);
+                    if (self.currentAudioJS.paused) {
+                        self._setAllAudioPause();
+                        self.currentAudioJS.play()
+                        self.calCountdown(self.currentAudio);
+                        $(this).hasClass('mp3_play') && $(this).removeClass('mp3_play').addClass('mp3_pause');
+                        return
+                    }
+                    self.currentAudioJS.pause();
+                    $(this).hasClass('mp3_pause') && $(this).removeClass('mp3_pause').addClass('mp3_play')
+                })
+            })
+        },
+        _setAllAudioPause: function() {
+            var self = this;
+            self.allAudio_P.each(function(idx) {
+                var audio = $(this).find('audio[data-id]').get(0);
+                if (!audio.paused) {
+                    audio.pause();
+                    $(audio).parent().hasClass('mp3_pause') && $(this).removeClass('mp3_pause').addClass('mp3_play')
+                }
+            })
+        },
+        _setCurrentAudioInfo: function(audiop) {
+            var self = this;
+            self.currentAudio = audiop.find("audio[data-id]");
+            self.currentAudioJS = self.currentAudio.get(0);
+            self.currentAudioId = audiop.attr('data-id');
+            self.currentAudioUrl = audiop.attr('data-url');;
+            self.currentAudioOriginalUrl = audiop.attr('data-original-url');;
+            self.currentAudioDuration = audiop.attr('data-duration');;
+            self.currentAudioSpanChild = audiop.find("span.el-audio-duration");
+        },
+        updateRemainTime: function(audio, second) {
+            var s = this._formatDuration(second);
+            audio.siblings('span.el-audio-duration').text(s);
+        },
+        calCountdown: function(audio) {
+            var self = this;
+            audio.on("timeupdate", function() {
+                var all = this.duration;
+                var curr = this.currentTime;
+                var diff = parseInt(all - curr);
+                if (diff <= 0) {
+                    audio.get(0).pause();
+                    self.updateRemainTime(audio, 0);
+                    self.initSingleAudio();
+                } else {
+                    if (!isNaN(diff)) {
+                        self.updateRemainTime(audio, diff)
+                    }
+                }
+            })
+        },
+        initSingleAudio: function() {
+            var self = this;
+            setTimeout(function() {
+                var parent = self.currentAudio.parent();
+                parent.hasClass('mp3_pause') && parent.removeClass('mp3_pause').addClass('mp3_play');
+                self.currentAudioSpanChild.text(self._formatDuration(self.currentAudioDuration));
+                self.currentAudio.attr('src', self.currentAudioUrl)
+            }, 1000)
+        },
+        _formatDuration: function(second) {
+            var m = Math.floor(second / 60);
+            m = m < 10 ? ('0' + m) : m;
+            var s = parseInt(second % 60);
+            s = s < 10 ? ('0' + s) : s;
+            return m + ':' + s;
+        },
+    }
+    window.m_audio = m_audio;
+}(window))
